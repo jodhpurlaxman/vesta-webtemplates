d17eff4bf53c864aea8cc5b77bfbb8aee90bcc35
Laxman Singh <jodhpur.laxman@gmail.com>
2022-04-10 22:23:58 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@
 #chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-56.sh
 
 ##==============================================================================================
-apt-get install php7.0-apcu php7.0-mbstring php7.0-bcmath php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-intl php7.0-mcrypt php7.0-mysql php7.0-soap php7.0-xml php7.0-zip php7.0-memcache php7.0-memcached php7.0-zip
+apt-get -y install php7.0-apcu php7.0-mbstring php7.0-bcmath php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-intl php7.0-mcrypt php7.0-mysql php7.0-soap php7.0-xml php7.0-zip php7.0-memcache php7.0-memcached php7.0-zip
 update-rc.d php7.0-fpm defaults
 a2enconf php7.0-fpm
 systemctl restart apache2
@@ -29,7 +29,7 @@ wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP
 wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP-70/PHP-FPM-70-DEFAULT.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70-DEFAULT.sh
 chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-70-*.sh
 
-apt-get install php7.1-apcu php7.1-mbstring php7.1-bcmath php7.1-cli php7.1-curl php7.1-fpm php7.1-gd php7.1-intl php7.1-mcrypt php7.1-mysql php7.1-soap php7.1-xml php7.1-zip php7.1-memcache php7.1-memcached php7.1-zip
+apt-get -y install php7.1-apcu php7.1-mbstring php7.1-bcmath php7.1-cli php7.1-curl php7.1-fpm php7.1-gd php7.1-intl php7.1-mcrypt php7.1-mysql php7.1-soap php7.1-xml php7.1-zip php7.1-memcache php7.1-memcached php7.1-zip
 update-rc.d php7.1-fpm defaults
 a2enconf php7.1-fpm
 systemctl restart apache2
@@ -47,7 +47,7 @@ wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP
 wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP-71/PHP-FPM-71-DEFAULT.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71-DEFAULT.sh
 chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-71-*.sh
 
-apt-get install php7.2-apcu php7.2-mbstring php7.2-bcmath php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-intl php7.2-mysql php7.2-soap php7.2-xml php7.2-zip php7.2-memcache php7.2-memcached php7.2-zip
+apt-get -y install php7.2-apcu php7.2-mbstring php7.2-bcmath php7.2-cli php7.2-curl php7.2-fpm php7.2-gd php7.2-intl php7.2-mysql php7.2-soap php7.2-xml php7.2-zip php7.2-memcache php7.2-memcached php7.2-zip
 update-rc.d php7.2-fpm defaults
 a2enconf php7.2-fpm
 systemctl restart apache2
@@ -65,7 +65,7 @@ wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP
 wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP-72/PHP-FPM-72-DEFAULT.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72-DEFAULT.sh
 chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-72-*.sh
 
-apt-get install php7.3-apcu php7.3-mbstring php7.3-bcmath php7.3-cli php7.3-curl php7.3-fpm php7.3-gd php7.3-intl php7.3-mysql php7.3-soap php7.3-xml php7.3-zip php7.3-memcache php7.3-memcached php7.3-zip
+apt-get -y install php7.3-apcu php7.3-mbstring php7.3-bcmath php7.3-cli php7.3-curl php7.3-fpm php7.3-gd php7.3-intl php7.3-mysql php7.3-soap php7.3-xml php7.3-zip php7.3-memcache php7.3-memcached php7.3-zip
 update-rc.d php7.3-fpm defaults
 a2enconf php7.3-fpm
 systemctl restart apache2
@@ -83,7 +83,7 @@ wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP
 wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP-73/PHP-FPM-73-DEFAULT.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-DEFAULT.sh
 chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-73-*.sh
 
-apt-get install php7.4-apcu php7.4-mbstring php7.4-bcmath php7.4-cli php7.4-curl php7.4-fpm php7.4-gd php7.4-intl php7.4-mysql php7.4-soap php7.4-xml php7.4-zip php7.4-memcache php7.4-memcached php7.4-zip
+apt-get -y install php7.4-apcu php7.4-mbstring php7.4-bcmath php7.4-cli php7.4-curl php7.4-fpm php7.4-gd php7.4-intl php7.4-mysql php7.4-soap php7.4-xml php7.4-zip php7.4-memcache php7.4-memcached php7.4-zip
 update-rc.d php7.4-fpm defaults
 a2enconf php7.4-fpm
 systemctl restart apache2
@@ -101,7 +101,7 @@ wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP
 wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP-74/PHP-FPM-74-DEFAULT.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-74-DEFAULT.sh
 chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-74-*.sh
 
-apt-get install php8.0-apcu php8.0-mbstring php8.0-bcmath php8.0-cli php8.0-curl php8.0-fpm php8.0-gd php8.0-intl php8.0-mysql php8.0-soap php8.0-xml php8.0-zip php8.0-memcache php8.0-memcached php8.0-zip
+apt-get -y install php8.0-apcu php8.0-mbstring php8.0-bcmath php8.0-cli php8.0-curl php8.0-fpm php8.0-gd php8.0-intl php8.0-mysql php8.0-soap php8.0-xml php8.0-zip php8.0-memcache php8.0-memcached php8.0-zip
 update-rc.d php8.0-fpm defaults
 a2enconf php8.0-fpm
 systemctl restart apache2
@@ -117,7 +117,7 @@ wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP
 wget https://raw.githubusercontent.com/jodhpurlaxman/vesta-webtemplates/main/PHP-80/PHP-FPM-80-DEFAULT.sh -O /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80-DEFAULT.sh
 chmod a+x /usr/local/vesta/data/templates/web/apache2/PHP-FPM-80-*.sh
 
-apt-get install php8.1-apcu php8.1-mbstring php8.1-bcmath php8.1-cli php8.1-curl php8.1-fpm php8.1-gd php8.1-intl php8.1-mysql php8.1-soap php8.1-xml php8.1-zip php8.1-memcache php8.1-memcached php8.1-zip
+apt-get -y install php8.1-apcu php8.1-mbstring php8.1-bcmath php8.1-cli php8.1-curl php8.1-fpm php8.1-gd php8.1-intl php8.1-mysql php8.1-soap php8.1-xml php8.1-zip php8.1-memcache php8.1-memcached php8.1-zip
 update-rc.d php8.1-fpm defaults
 a2enconf php8.1-fpm
 systemctl restart apache2
